9e3d8f88602a8283060e537562aadfda5e40dcc6
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-07 19:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/emilyyz92/repos');
   req.send();
 }
