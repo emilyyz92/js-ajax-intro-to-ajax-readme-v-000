51ffbe51c5ee0883ae05d08fa840459bcfe1f116
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-07 19:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', 'https://api.github.com/users/emilyyz92/repos');
   req.send();
 }
